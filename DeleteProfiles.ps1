c2d39acf99a9d6c12501ec31328da9464fe86fda
mcaulr09 <37441018+mcaulr09@users.noreply.github.com>
2018-07-27 21:05:42 +0800

    Profiles script
    Deletes prrofiles older than 180 days Delprofv2 and backups profile.

---
+++ DeleteProfiles.ps1
@@ -0,0 +1,29 @@
+﻿### Please note that You will need to enable the PSRemoting Service by running Enable-PSRemoting on machine in Naverisk - Powershell
+### Get computers ###
+
+$Computer = Read-Host "Enter Computer Name"
+
+$Computers = (Get-ADComputer $Computer).Name
+
+set /p id=$Computers
+
+### Copy Delprofv2 to machines ###
+
+Get-Service -Name RemoteRegistry -ComputerName $Computers | Start-service
+
+robocopy C:\Temp\Delprof2 \\$Computers\c$\Temp\Delprof2\ /r:0 /w:0
+
+### Start WinRM Service on remote machine ###
+
+Get-Service -Name WinRM -ComputerName $Computers | Start-service
+
+### Launch Delprofv2 to delete profileas onder than 180 days using NTUSER.DAT###
+### When determining profile age for /d, use the file NTUSER.INI
+#### instead of NTUSER.DAT for age calculation 
+ 
+Invoke-Command -ComputerName $Computers -ScriptBlock { c:\Temp\Delprof2\DelProf2.exe /q /ed:admin* /ed:deep* /d:180 /i}
+
+### When determining profile age for /d, use the file NTUSER.INI
+### instead of NTUSER.DAT for age calculation
+
+Invoke-Command -ComputerName $Computers -ScriptBlock { c:\Temp\DelProf2.exe /l /ed:admin* /ed:deep* /ntuserini}
---
+++ test-backupprofile.ps1
@@ -0,0 +1,34 @@
+﻿$destination = "\\qnap-451\MCAULR09"
+
+$folder = "Desktop",
+"Downloads",
+"Favorites",
+"Documents",
+"Music",
+"Pictures",
+"Videos",
+"AppData\Local\Mozilla",
+"AppData\Local\Google",
+"AppData\Roaming\Mozilla"
+
+###############################################################################################################
+
+$username = gc env:username
+$userprofile = gc env:userprofile
+$appData = gc env:localAPPDATA
+
+([IO.Directory]::Exists($destination + "\" + "Profile" + "\"))
+
+###### Backup Data section ########
+	write-host -ForegroundColor green "Backing up data from local machine for $username"
+	
+	foreach ($f in $folder)
+	{	
+		$currentLocalFolder = $userprofile + "\" + $f
+		$currentRemoteFolder = $destination + "\" + "Profile" + "\" + $f
+		$currentFolderSize = (Get-ChildItem -ErrorAction silentlyContinue $currentLocalFolder -Recurse -Force | Measure-Object -ErrorAction silentlyContinue -Property Length -Sum ).Sum / 1MB
+		$currentFolderSizeRounded = [System.Math]::Round($currentFolderSize)
+		write-host -ForegroundColor cyan "  $f... ($currentFolderSizeRounded MB)"
+		robocopy $currentLocalFolder $currentRemoteFolder /S /E /XO
+	}
+	
